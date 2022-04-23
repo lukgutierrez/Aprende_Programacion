75159c76a97e0f61aac1981ec3f3826076e497f8
Luciano Gutierrez <lucianogutierrezagustin@gmail.com>
2022-04-23 17:56:03 -0300

    Diseño Pantalla

--- lib/src/pages/plataformas.dart
+++ lib/src/pages/plataformas.dart
@@ -12,38 +12,65 @@ class _PaginaInformativaState extends State<PaginaInformativa> {
       body: Column(
         children: [
           Stack(
+            clipBehavior: Clip.none,
             children: [
               Container(
                 width: 400.0,
                 height: 200.0,
-                color: Colors.blue,
+                color: Colors.red,
+              ),
+              Positioned(
+                left: 10.0,
+                bottom: -80,
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.blue,
+                          width: 50.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 250,
+                      child: ListTile(
+                        title: Text("PLATZI"),
+                        subtitle: Text("#NUNCAPARESDEAPRENDER"),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
-//CONSEJO CUANDO TRABAJEMOS CON ROW Y LISTTILE.
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.orange,
-                    width: 40.0,
-                  ),
-                ),
-              ),
-//Al Listtile tenemos que expecificar el tamaño que va a ocupar en el ROW.
-//https://stackoverflow.com/questions/62041954/placing-listtiles-in-a-row (Respuesta)
-              Expanded(
-                child: ListTile(
-                  title: Text("PLATZI"),
-                  subtitle: Text("#NUNCAPARESDEAPRENDER"),
-                ),
-              ),
-            ],
-          ),
+          Divider(height: 80.0,color: Colors.transparent,),
+          Contenido(),
+          Contenido(),
+          Contenido(),
         ],
       ),
+    );
+  }
+}
+
+class Contenido extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.abc_outlined),
+          // leading: ImageIcon(
+          //   AssetImage("assets/icon/hlep.png"),
+          //   color: Colors.black,
+          //   size: 80,
+          // ),
+          subtitle: Text("Esto sucede cuando ListView no tiene una altura restringida, lo que hace que tenga una altura infinita, puede resolver esto usando dos soluciones"),
+          title: Text("DESCRIPCIÓN"),
+        ),
+      ],
     );
   }
 }
